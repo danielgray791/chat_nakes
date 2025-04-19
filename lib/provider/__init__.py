e4451c695b4c051e2de977ce7acbd32c6fbce2de
danielgray791 <danielgray791@outlook.com>
2025-04-19 22:10:17 +0700

    Update __init__.py

--- lib/provider/__init__.py
+++ lib/provider/__init__.py
@@ -181,10 +181,12 @@ providers = {
         item_name="scira",
         default_model="Grok 3 dot 0",
         models=[
-            Model(number=1, name="Claude 3 dot 7 Sonnet", id="scira-claude", vision=True),
-            Model(number=2, name="Grok 2 dot 0 Vision", id="scira-vision", vision=True),
-            Model(number=3, name="Grok 3 dot 0 mini", id="scira-grok-3-mini", vision=False),
-            Model(number=4, name="Grok 3 dot 0", id="scira-default", vision=False),
+            Model(number=1, name="GPT 4 dot 1 Mini", id="scira-4.1-mini", vision=True),
+            Model(number=2, name="GPT o4 Mini", id="scira-o4-mini", vision=True),
+            Model(number=3, name="Grok 2 dot 0 Vision", id="scira-vision", vision=True),
+            Model(number=4, name="Grok 3 dot 0 mini", id="scira-grok-3-mini", vision=True),
+            Model(number=5, name="Grok 3 dot 0", id="scira-default", vision=True),
+            Model(number=6, name="Gemini 2 dot 5 Flash", id="scira-google", vision=True),
         ]
     )
 }
